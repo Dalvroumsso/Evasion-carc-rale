7a4a9f90e7393f637dff76f3cdf0816577ac5d0d
Dalvroumsso <nicolasd.dalmasso@gmail.com>
2026-02-18 14:36:01 +0100

    Revise index.html for Blackridge: Prison Riot
    Updated HTML structure and added new styles.

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="UTF-8" />
-  <link rel="icon" type="image/svg+xml" href="https://base44.com/logo_v2.svg" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Prison Riot</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blackridge: Prison Riot</title>
 
-  <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 
-  <script crossorigin src="https://unpkg.com/react@18/umd/react.development.js"></script>
-  <script crossorigin src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
-  
-  <style>
-    /* Petit correctif pour les barres de défilement du journal */
-    .custom-scrollbar::-webkit-scrollbar { width: 4px; }
-    .custom-scrollbar::-webkit-scrollbar-track { background: transparent; }
-    .custom-scrollbar::-webkit-scrollbar-thumb { background: #374151; border-radius: 10px; }
-  </style>
+    <link rel="stylesheet" href="style.css">
+
+    <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+
+    <style>
+        /* Un petit fix pour s'assurer que le fond occupe tout l'écran */
+        body {
+            background-color: #0a0a0c;
+            min-height: 100vh;
+        }
+    </style>
 </head>
-<body class="bg-gray-950 text-white selection:bg-blue-500/30">
-  <div id="root"></div>
+<body>
 
-  <script src="gameData.js"></script>
-  <script src="introData.js"></script> 
-  <script src="StartScreen.js"></script>
-  <script src="IntroScene.js"></script> 
-  <script src="RoomGraphics.js"></script>
-  <script src="RoomView.js"></script>
+    <div id="root"></div>
 
-  <script src="Game.js"></script>
+    <script src="gameData.js"></script>
+    <script src="introData.js"></script>
 
-  <script src="main.js"></script>
+    <script type="text/babel" src="StartScreen.js"></script>
+    <script type="text/babel" src="IntroScene.js"></script>
+    <script type="text/babel" src="RoomGraphics.js"></script>
+    <script type="text/babel" src="RoomView.js"></script>
+    <script type="text/babel" src="Game.js"></script>
+
+    <script type="text/babel" src="main.js"></script>
+
 </body>
 </html>
